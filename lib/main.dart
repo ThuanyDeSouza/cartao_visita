30b3f3a3065c1888bc4f019f18b53bf08c0f10dc
ThuanyDeSouza <thuanydesouza26@gmail.com>
2022-08-17 22:11:22 -0300

    Final da aula 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-  )
-  ));
+  runApp(const BusinessCard());
 }
 
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Container(
+              child: const Text("Hello World"),
+              color: Colors.white,
+              width: 100,
+              height: 100,
+              //margin: EdgeInsets.all(30),
+              //margin: EdgeInsets.symmetric(
+              //  vertical: 10,
+              //  horizontal: 50,
+              //),
+              margin: EdgeInsets.fromLTRB(30, 10, 20, 50),
+            ),
+          ),
+        )
+    );
+  }
+}
