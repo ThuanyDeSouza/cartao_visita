c1c08bea562c82e26903f52ded63d38346009610
ThuanyDeSouza <thuanydesouza26@gmail.com>
2022-08-24 22:10:35 -0300

    Aula 4 - atividade 2 - cartão de visitas completo

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,63 @@ class BusinessCard extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/perfil.jfif'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.yellow,
-                      width: 100,
-                      height: 100,
-                      alignment: Alignment.center,
-                    ),Container(
-                      color: Colors.green,
-                      width: 100,
-                      height: 100,
-                      alignment: Alignment.center,
+                Text('Thuany de Souza',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                Text('Software developer',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5,
+                )
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading:
+                        Icon(Icons.phone,
+                        color: Colors.teal[900]),
+                    title: Text('47 988147447',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      ),
                     ),
-                  ],
+                  ),
                 ),
-                Container(
-                  color: Colors.blue,
-                  width: 100,
+                Card(
+                  child: ListTile(
+                    leading:
+                        Icon(Icons.email,
+                        color: Colors.teal[900]),
+                    title: Text('thuanydesouza26@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
